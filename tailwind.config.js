62518dd14744823c92f77372d1f29e4f62f50382
TheChickenKnight <81781990+TheChickenKnight@users.noreply.github.com>
2022-06-04 21:19:18 -0400

    button yes

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,13 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        violet: {
+          900: "#270033"
+        }
+      }
+    },
   },
   plugins: [],
 }
